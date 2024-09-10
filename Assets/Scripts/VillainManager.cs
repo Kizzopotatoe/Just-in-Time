410400369c60f62786ba6c11de02af98ca23487b
shivani-520 <shivani_2002@icloud.com>
2024-09-10 20:19:25 +0100

    Villain End Sequence
    Work in progress; made some more effects + flashing lights and camera shake.

--- Assets/Scripts/VillainManager.cs
+++ Assets/Scripts/VillainManager.cs
@@ -1,29 +1,57 @@
 using System.Collections;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Rendering;
 
 public class VillainManager : MonoBehaviour
 {
     [SerializeField] private GameObject villain;
+    private GameObject player;
     [SerializeField] private CinemachineVirtualCamera villainVirtualCamera;
+
+    [Header("Movement")]
     [SerializeField] private float moveDuration = 5f;
     [SerializeField] private float yPositionToStop = 4f;
 
+    [Header("Effects")]
     [SerializeField] private GameObject lightningEffect;
+
+    [Header("Flashing Lights")]
+    [SerializeField] private GameObject lightsParentObject;
+    [SerializeField] private Light[] lights;
+    [SerializeField] private float flashSpeed = 1f;
+    [SerializeField] private float maxIntensity = 2f;
+    private bool isOn = false;
+    private float timer = 0f;
 
     private void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Start()
+    {
         villain.SetActive(false);
         villainVirtualCamera.gameObject.SetActive(false);
+        lightsParentObject.SetActive(false);
+
+        if (lights.Length == 0)
+        {
+            Debug.Log("No Lights assigned in villain manager lights array");
+        }
     }
 
     public void EndSequence()
     {
+        player.SetActive(false);
         StartCoroutine(VillainSequence()); // called when time runs out
+        FlashLight();
     }
 
     IEnumerator VillainSequence()
     {
+        lightsParentObject.SetActive(true);
+
         villain.SetActive(true);
         villainVirtualCamera.gameObject.SetActive(true);
 
@@ -47,6 +75,27 @@ public class VillainManager : MonoBehaviour
         // when destroying is done then display game over screen
         GameManager.instance.levelFailedMenu.SetActive(true);
         Time.timeScale = 0;
-
     }
+
+    private void FlashLight()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= flashSpeed)
+        {
+            isOn = !isOn;
+
+            foreach(Light light in lights)
+            {
+                if(light != null)
+                {
+                    light.intensity = isOn ? maxIntensity : 0f;
+                }
+            }
+
+            timer = 0f;
+        }
+    }
+
+    
 }
