1945794c5e1fe60ee8111d403c5d1f19bcd11d0d
shivani-520 <shivani_2002@icloud.com>
2024-09-12 16:46:08 +0100

    Level Select Buttons Update

--- Assets/Scripts/LevelSelect/LevelMenu.cs
+++ Assets/Scripts/LevelSelect/LevelMenu.cs
@@ -8,7 +8,7 @@ public class LevelMenu : MonoBehaviour
 
     private void Awake()
     {
-        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 0);
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
         for (int i = 0; i < buttons.Length; i++)
         {
             buttons[i].interactable = false;
@@ -24,10 +24,5 @@ public class LevelMenu : MonoBehaviour
         // how scenes are named
         string levelName = "Level_" + levelId;
         SceneManager.LoadScene(levelName);
-    }
-
-    public void ResetGame()
-    {
-        
     }
 }
